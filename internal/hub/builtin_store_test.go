f10928415f1a2f22ca0e95f01bc1b928dad23fa3
GatewayJ <835269233@qq.com>
2026-05-25 18:15:34 +0800

    chore: update builtin picoclaw template image (#110)

--- internal/hub/builtin_store_test.go
+++ internal/hub/builtin_store_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	testBuiltinPicoClawImage = "opencsg-registry.cn-beijing.cr.aliyuncs.com/opencsghq/picoclaw:2026.5.18"
+	testBuiltinPicoClawImage = "opencsg-registry.cn-beijing.cr.aliyuncs.com/opencsghq/picoclaw:2026.5.22"
 	testBuiltinOpenClawImage = "opencsg-registry.cn-beijing.cr.aliyuncs.com/opencsghq/openclaw:20260518.1-csgclaw"
 )
 
